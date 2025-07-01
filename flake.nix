6e1f13a7ae57b1032616f31918f7f189dcd1ee0f
Cameron Yule <cameron@cameronyule.com>
2025-07-01 13:41:53 +0100

    build: nix environment for Python

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            packages = [
+              (python313.withPackages (
+                ps: with ps; [
+                  uv
+                ]
+              ))
+            ];
+            shellHook = ''
+              export UV_PYTHON_PREFERENCE="only-system"
+              export UV_PYTHON=${pkgs.python313}
+              uv venv bookmark-processor/.venv
+            '';
+          };
+
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    );
+}
