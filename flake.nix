b55cdbc8992c0467f51c047ca48fdb8a7f85e628
Cameron Yule <cameron@cameronyule.com>
2025-07-01 14:37:03 +0100

    build: automatic formatting and linting

--- flake.nix
+++ flake.nix
@@ -1,40 +1,55 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    # keep-sorted start block=yes
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+    };
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    # keep-sorted end
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        devShells.default =
-          with pkgs;
-          mkShell {
-            packages = [
-              (python313.withPackages (
-                ps: with ps; [
+  outputs = inputs @ {
+    # keep-sorted start
+    flake-parts,
+    nixpkgs,
+    self,
+    treefmt-nix,
+    # keep-sorted end
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem = {pkgs, ...}: let
+        treefmtEval = treefmt-nix.lib.evalModule pkgs ./internal/nix/treefmt.nix;
+      in {
+        formatter = treefmtEval.config.build.wrapper;
+        checks.formatting = treefmtEval.config.build.check self;
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            (python313.withPackages (
+              ps:
+                with ps; [
                   uv
                 ]
-              ))
-            ];
-            shellHook = ''
-              export UV_PYTHON_PREFERENCE="only-system"
-              export UV_PYTHON=${pkgs.python313}
-              uv venv --allow-existing bookmark-processor/.venv
-              source bookmark-processor/.venv/bin/activate
-            '';
-          };
-
-        formatter = pkgs.nixfmt-rfc-style;
-      }
-    );
+            ))
+          ];
+          shellHook = ''
+            export UV_PYTHON_PREFERENCE="only-system"
+            export UV_PYTHON=${pkgs.python313}
+            uv venv --allow-existing bookmark-processor/.venv
+            source bookmark-processor/.venv/bin/activate
+          '';
+        };
+      };
+    };
 }
---
+++ internal/nix/treefmt.nix
@@ -0,0 +1,48 @@
+_: {
+  projectRootFile = "flake.nix";
+
+  programs = {
+    # keep-sorted start block=yes
+    actionlint = {
+      enable = true;
+      priority = 5;
+    };
+    alejandra = {
+      enable = true;
+      priority = 4;
+    };
+    deadnix = {
+      enable = true;
+      excludes = [
+        "flake.nix"
+      ];
+      priority = 0;
+    };
+    keep-sorted = {
+      enable = true;
+      priority = 3;
+    };
+    ruff-check = {
+      enable = true;
+      priority = 7;
+    };
+    ruff-format = {
+      enable = true;
+      priority = 6;
+    };
+    statix = {
+      enable = true;
+      priority = 1;
+    };
+    # keep-sorted end
+  };
+
+  settings = {
+    allow-missing-formatter = false;
+    on-unmatched = "fatal";
+    verbose = 2;
+    global.excludes = [
+      ".editorconfig"
+    ];
+  };
+}
