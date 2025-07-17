97791f0e71bb8f65c543ad280c8eb791b29a8f66
Cameron Yule <cameron@cameronyule.com>
2025-07-17 15:50:34 +0100

    build: dependency on ollama

--- flake.nix
+++ flake.nix
@@ -36,6 +36,7 @@
 
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
+            ollama
             (python313.withPackages (
               ps:
                 with ps; [
